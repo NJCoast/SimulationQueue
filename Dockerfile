860fef3143edeadbb1fcab0c47b87296362419c7
James Sweet <james.sweet88@googlemail.com>
2018-03-05 16:33:34 -0500

    Add code to watch S3 bucket and execute job on new file containing input_params.json

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM golang:1.9.4
-WORKDIR /go/src/github.com/omegaice/HurricaneSimulationQueue/
+WORKDIR /go/src/github.com/NJCoast/SimulationQueue/
 COPY main.go .
-RUN go get k8s.io/client-go/... && CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o HurricaneSimulationQueue .
+RUN go get k8s.io/client-go/... && \
+    go get github.com/aws/aws-sdk-go/aws && \
+    go get github.com/aws/aws-sdk-go/aws/awserr && \
+    go get github.com/aws/aws-sdk-go/aws/session && \
+    go get github.com/aws/aws-sdk-go/service/sqs && \
+    CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o SimulationQueue .
 
 FROM alpine:3.7  
+RUN apk add --no-cache ca-certificates
 WORKDIR /root/
-COPY --from=0 /go/src/github.com/omegaice/HurricaneSimulationQueue/HurricaneSimulationQueue .
-CMD ["./HurricaneSimulationQueue"] 
+COPY --from=0 /go/src/github.com/NJCoast/SimulationQueue .
+CMD ["./SimulationQueue", "-n", "simulation_queue"] 
--- main.go
+++ main.go
@@ -1,94 +1,154 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"flag"
 	"log"
+	"strings"
+	"encoding/json"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	corev1 "k8s.io/api/core/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/awserr"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
+type Records struct {
+	Data []Record `json:"Records"`
+}
+
+type Record struct {
+	Source string `json:"eventSource"`
+	Event string `json:"eventName"`
+	Data S3Record `json:"s3"`
+}
+
+type S3Record struct {
+	Object S3ObjectRecord `json:"object"`
+}
+
+type S3ObjectRecord struct {
+	Name string `json:"key"`
+	Size int `json:"size"`
+	eTag string `json:"eTag"`
+}
+
 func main() {
-	// creates the in-cluster config
+	var name string
+    flag.StringVar(&name, "n", "", "Queue name")
+    flag.Parse()
+
+    if len(name) == 0 {
+        flag.PrintDefaults()
+        log.Fatalln("Queue name required")
+    }
+
+	// Setup AWS
+    sess, err := session.NewSession(&aws.Config{
+        Region: aws.String("us-east-1")},
+	)
+	
+	svc := sqs.New(sess)
+	
+    resultURL, err := svc.GetQueueUrl(&sqs.GetQueueUrlInput{ QueueName: aws.String(name) })
+    if err != nil {
+        if aerr, ok := err.(awserr.Error); ok && aerr.Code() == sqs.ErrCodeQueueDoesNotExist {
+            log.Fatalf("Unable to find queue %q\n", name)
+        }
+        log.Fatalf("Unable to queue %q, %v.\n", name, err)
+	}
+
+	// Setup Kubernetes
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
-	// creates the clientset
+	
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		jobsClient := clientset.BatchV1().Jobs("default")
 
-		jobsList, err := jobsClient.List(metav1.ListOptions{})
+	for{
+		result, err := svc.ReceiveMessage(&sqs.ReceiveMessageInput{
+			QueueUrl: resultURL.QueueUrl,
+			AttributeNames: aws.StringSlice([]string{"SentTimestamp"}),
+			MaxNumberOfMessages: aws.Int64(1),
+			MessageAttributeNames: aws.StringSlice([]string{"ALL"}),
+			WaitTimeSeconds: aws.Int64(20),
+		})
 		if err != nil {
-			log.Fatalln(err)
+			log.Fatalf("Unable to receive message from queue %q, %v.\n", name, err)
 		}
 
-		// Loop over all jobs and print their name
-		for i, job := range jobsList.Items {
-			fmt.Printf("Job %d: %s\n", i, job.Name)
-		}
+		if len(result.Messages) > 0 {
+			log.Printf("Received %d messages.\n", len(result.Messages))
+			log.Println(result.Messages)
 
-		var falseVal = false
+			var data Records
+			if err := json.NewDecoder(strings.NewReader(*result.Messages[0].Body)).Decode(&data); err != nil {
+				log.Println("error:", err)
+			}
+			
+			if data.Data[0].Source == "aws:s3" && data.Data[0].Event == "ObjectCreated:Put" && strings.Contains(data.Data[0].Data.Object.Name, "input_params.json") {
+				jobsClient := clientset.BatchV1().Jobs("default")
 
-		batchJob := &batchv1.Job{
-			TypeMeta: metav1.TypeMeta{
-				Kind:       "Job",
-				APIVersion: "v1",
-			},
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   "k8sexp-testjob",
-				Labels: make(map[string]string),
-			},
-			Spec: batchv1.JobSpec{
-				// Optional: Parallelism:,
-				// Optional: Completions:,
-				// Optional: ActiveDeadlineSeconds:,
-				// Optional: Selector:,
-				// Optional: ManualSelector:,
-				Template: corev1.PodTemplateSpec{
+				var falseVal = false
+				batchJob := &batchv1.Job{
+					TypeMeta: metav1.TypeMeta{
+						Kind:       "Job",
+						APIVersion: "v1",
+					},
 					ObjectMeta: metav1.ObjectMeta{
-						Name:   "k8sexp-testpod",
+						GenerateName:   "runup-",
 						Labels: make(map[string]string),
 					},
-					Spec: corev1.PodSpec{
-						InitContainers: []corev1.Container{}, // Doesn't seem obligatory(?)...
-						Containers: []corev1.Container{
-							{
-								Name:    "k8sexp-testimg",
-								Image:   "perl",
-								Command: []string{"sleep", "30"},
-								SecurityContext: &corev1.SecurityContext{
-									Privileged: &falseVal,
+					Spec: batchv1.JobSpec{
+						Template: corev1.PodTemplateSpec{
+							ObjectMeta: metav1.ObjectMeta{
+								GenerateName:   "runup-",
+								Labels: make(map[string]string),
+							},
+							Spec: corev1.PodSpec{
+								InitContainers: []corev1.Container{},
+								Containers: []corev1.Container{
+									{
+										Name:    "runup",
+										Image:   "perl",
+										Command: []string{"sleep", "30"},
+										SecurityContext: &corev1.SecurityContext{
+											Privileged: &falseVal,
+										},
+										ImagePullPolicy: corev1.PullPolicy(corev1.PullIfNotPresent),
+										Env:             []corev1.EnvVar{},
+										VolumeMounts:    []corev1.VolumeMount{},
+									},
 								},
-								ImagePullPolicy: corev1.PullPolicy(corev1.PullIfNotPresent),
-								Env:             []corev1.EnvVar{},
-								VolumeMounts:    []corev1.VolumeMount{},
+								RestartPolicy:    corev1.RestartPolicyOnFailure,
+								Volumes:          []corev1.Volume{},
+								ImagePullSecrets: []corev1.LocalObjectReference{},
 							},
 						},
-						RestartPolicy:    corev1.RestartPolicyOnFailure,
-						Volumes:          []corev1.Volume{},
-						ImagePullSecrets: []corev1.LocalObjectReference{},
 					},
-				},
-			},
-			// Optional, not used by pach: JobStatus:,
+				}
+
+				newJob, err := jobsClient.Create(batchJob)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				log.Println("New job name: ", newJob.Name)
+			}
+
+			if _, err := svc.DeleteMessage(&sqs.DeleteMessageInput{QueueUrl: resultURL.QueueUrl, ReceiptHandle: result.Messages[0].ReceiptHandle }); err != nil {
+				log.Println("Delete Error", err)
+				return
+			}
 		}
-
-		newJob, err := jobsClient.Create(batchJob)
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Println("New job name: ", newJob.Name)
-
-		time.Sleep(60 * time.Second)
 	}
 }
